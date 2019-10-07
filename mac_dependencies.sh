a4591bdca8cad9011ddf30cc5d94aaa96ef3de83
black-bitcoin <55807122+black-bitcoin@users.noreply.github.com>
2019-10-07 12:41:25 +0200

    Mac Dependencies

---
+++ mac_dependencies.sh
@@ -0,0 +1,14 @@
+brew install autoconf
+brew install automake
+brew install pkg-config
+brew install protobuf
+brew install boost@1.57 --cc=clang 
+brew link boost@1.57 --force
+brew install berkeley-db@4 
+brew link berkeley-db@4 --force
+brew install openssl 
+brew install miniupnpc
+brew install libevent
+brew install qt5  
+brew link qt5 --force
+brew install qrencode
